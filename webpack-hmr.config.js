772ba61f48755c037c3499a2ab30f2d603f0fc6b
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-12-13 00:54:29 +0900

    hot reload를 위한 webpack 패키지 설치 및 적용

---
+++ webpack-hmr.config.js
@@ -0,0 +1,27 @@
+/* hot reload를 위한 webpack 패키지 설정 파일 */
+
+const nodeExternals = require('webpack-node-externals');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+
+module.exports = function (options, webpack) {
+  return {
+    ...options,
+    entry: ['webpack/hot/poll?100', options.entry],
+    externals: [
+      nodeExternals({
+        allowlist: ['webpack/hot/poll?100'],
+      }),
+    ],
+    plugins: [
+      ...options.plugins,
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.WatchIgnorePlugin({
+        paths: [/\.js$/, /\.d\.ts$/],
+      }),
+      new RunScriptWebpackPlugin({
+        name: options.output.filename,
+        autoRestart: false,
+      }),
+    ],
+  };
+};
