afb51d31a81967db5f987cbf62ba3d0f50fede73
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-12-25 18:22:16 +0900

    posts 부분 swagger 및 메모장 업데이트

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -7,7 +7,7 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PostsService } from './posts.service';
 import { LoggedInGuard } from 'src/auth/logged-in.guard';
 import { PostingRequestDto } from './dto/posting.request.dto';
@@ -18,7 +18,8 @@ export class PostsController {
   constructor(private postsService: PostsService) {}
 
   // 글 작성
-  // @UseGuards(LoggedInGuard)
+  @ApiOperation({ summary: '게시글 작성' })
+  @UseGuards(LoggedInGuard)
   @Post()
   async createPost(@Body() body: PostingRequestDto) {
     await this.postsService.createPost(body.title, body.content);
@@ -26,20 +27,24 @@ export class PostsController {
   }
 
   // 전체 글 가져오기
+  @ApiOperation({ summary: '전체 게시글 가져오기' })
   @Get()
   getAllPost() {}
 
   // 특정 글 가져오기
+  @ApiOperation({ summary: '특정 게시글 가져오기' })
   @Get()
   getSpecificPost() {}
 
   // 글 수정
-  // @UseGuards(LoggedInGuard)
+  @ApiOperation({ summary: '게시글 수정' })
+  @UseGuards(LoggedInGuard)
   @Patch()
   updatePost() {}
 
   // 글 삭제
-  // @UseGuards(LoggedInGuard)
+  @ApiOperation({ summary: '게시글 삭제' })
+  @UseGuards(LoggedInGuard)
   @Delete()
   deletePost() {}
 }
