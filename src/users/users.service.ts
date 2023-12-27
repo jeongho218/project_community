33a1be4324b773a259339b057dfa7c98a72363d2
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-12-27 23:41:16 +0900

    트랜잭션 설정

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,25 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from './users.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users) private usersRepository: Repository<Users>,
+    private dataSource: DataSource,
   ) {}
 
   // 회원가입 api
   async signUp(email: string, nickname: string, password: string) {
-    const user = await this.usersRepository.findOne({
-      where: { email: email },
-    });
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect(); // DB 연결
+    await queryRunner.startTransaction(); // 트랜잭션 시작
+
+    const user = await queryRunner.manager
+      .getRepository(Users)
+      .findOne({ where: { email: email } });
 
     // 중복되는 이메일인 경우 에러 처리
     if (user) {
@@ -24,11 +29,27 @@ export class UsersService {
     // 패스워드 암호화
     const hashedPassword = await bcrypt.hash(password, 12);
 
+    try {
+      await queryRunner.manager
+        .getRepository(Users)
+        .save({ email: email, nickname: nickname, password: hashedPassword });
+
+      await queryRunner.commitTransaction();
+      return true;
+    } catch (error) {
+      console.log(error);
+      await queryRunner.rollbackTransaction(); // 트랜잭션 실행 전으로 롤백
+      throw error;
+    } finally {
+      await queryRunner.release(); // DB 연결 종료
+    }
+
+    // before
     // DB에 저장
-    await this.usersRepository.save({
-      email: email,
-      nickname: nickname,
-      password: hashedPassword,
-    });
+    // await this.usersRepository.save({
+    //   email: email,
+    //   nickname: nickname,
+    //   password: hashedPassword,
+    // });
   }
 }
