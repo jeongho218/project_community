9919bc6ac879a93a761f389da78d2a43cb71d37d
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2024-01-16 16:09:02 +0900

    불필요한 트랜잭션 과정 제거

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,18 +8,13 @@ import bcrypt from 'bcrypt';
 export class UsersService {
   constructor(
     @InjectRepository(Users) private usersRepository: Repository<Users>,
-    private dataSource: DataSource,
   ) {}
 
-  // 회원가입 api
+  // 회원가입 API
   async signUp(email: string, nickname: string, password: string) {
-    const queryRunner = this.dataSource.createQueryRunner();
-    await queryRunner.connect(); // DB 연결
-    await queryRunner.startTransaction(); // 트랜잭션 시작
-
-    const user = await queryRunner.manager
-      .getRepository(Users)
-      .findOne({ where: { email: email } });
+    const user = await this.usersRepository.findOne({
+      where: { email: email },
+    });
 
     // 중복되는 이메일인 경우 에러 처리
     if (user) {
@@ -29,19 +24,11 @@ export class UsersService {
     // 패스워드 암호화
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    try {
-      await queryRunner.manager
-        .getRepository(Users)
-        .save({ email: email, nickname: nickname, password: hashedPassword });
-
-      await queryRunner.commitTransaction(); // 트랜잭션 내 변경 내용 커밋 및 완료
-      return true;
-    } catch (error) {
-      console.log(error);
-      await queryRunner.rollbackTransaction(); // 트랜잭션 실행 전으로 롤백
-      throw error;
-    } finally {
-      await queryRunner.release(); // DB 연결 종료
-    }
+    // DB에 저장
+    await this.usersRepository.save({
+      email: email,
+      nickname: nickname,
+      password: hashedPassword,
+    });
   }
 }
